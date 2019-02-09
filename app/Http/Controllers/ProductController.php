946859d3944619155fb044e92f4e7b420710c0c9
aimeirak <aimeirak10@gmail.com>
2019-02-09 16:20:33 +0200

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Product;
+
+
+class ProductController extends Controller
+{
+    //
+    public function list(){
+
+    	$data = Product::all();
+    	return $data;
+    }
+    public function insert(Request $request)
+    {
+    	// print_r($request->input'id'));
+    	$product = new Product;
+    	$product->id = $request->input('id');
+    	$product->Name = $request->input('Name');
+    	$product->descrition = $request->input('descrition');
+    	$product->price = $request->input('price');
+    	$product->save(); 
+    }
+}
